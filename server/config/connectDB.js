595d7598fda03026f331d6c297e80da2203e9df8
azeem <114247991+azeemhub0677@users.noreply.github.com>
2026-01-09 22:36:35 +0530

    Update connectDB.js

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config()
-
+// hswffgdghf
 if(!process.env.MONGODB_URI){
     throw new Error(
         "Please provide MONGODB_URI in the .env file"
